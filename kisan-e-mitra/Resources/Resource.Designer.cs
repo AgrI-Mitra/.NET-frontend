16aa9c33a8aaf4f3b1b629ca01a6df1d8f596eae
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2023-08-21 16:55:07 +0530

    Fixed Hindi English Translations for Welcome and language change greetings

--- kisan-e-mitra/Resources/Resource.Designer.cs
+++ kisan-e-mitra/Resources/Resource.Designer.cs
@@ -747,7 +747,8 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Welcome to Kisan-eMitra! I am your personal assistant. Please let me know how can I help you?.
+        ///   Looks up a localized string similar to Welcome to Kisan-eMitra! I am your personal assistant. Please let me know how can I help you? 
+        ///You may select your preferred language..
         /// </summary>
         public static string message_welcome_greeting {
             get {
