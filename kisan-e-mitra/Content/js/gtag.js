cf244ec8b2c28b092440c47c19e642c6f6228b42
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2023-11-08 11:13:08 +0530

    Added Google Analytics Code

---
+++ kisan-e-mitra/Content/js/gtag.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'G-RCEZR4J24G');
+});
