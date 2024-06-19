f3cc29809906762863dd62144c726c0d28770a82
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2024-06-19 11:21:34 +0530

    Created bundle config for main chatbot javascript file, to prevent caching whenever file is updated.

--- kisan-e-mitra/App_Start/BundleConfig.cs
+++ kisan-e-mitra/App_Start/BundleConfig.cs
@@ -79,8 +79,8 @@ namespace KisanEMitra
                 "~/Content/js/bundle-post-body.min.js"
                 ));
 
-            //bundles.Add(new Bundle("~/bundles/chatbot-js").Include(
-            //    "~/Content/js/chatbot.js"));
+            bundles.Add(new Bundle("~/bundles/chatbot-js").Include(
+                "~/Content/js/chatbot.min.js"));
 
             BundleTable.EnableOptimizations = true;
         }
