6c79b1bcec7eddc42a8c58fb33ea2310549c5b8c
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2024-06-19 16:29:20 +0530

    Updated minified bundles

--- kisan-e-mitra/Content/js/bundle-post-body.js
+++ kisan-e-mitra/Content/js/bundle-post-body.js
@@ -1,23 +1,3 @@
-(function () {
-
-    // Listen scroll events
-    document.getElementById('message-list').addEventListener('scroll', function (event) {
-
-        //Hide header if srolled to bottom direction and make header visible again on scroll top top direction
-
-        if (event.target.scrollTop > 0 || event.target.scrollTop > 0) {
-            $('#appHeader').removeClass('d-flex');
-            $('#appHeader').addClass('d-none');
-            $('#message-list').addClass('without-header');
-            $('#appHeader').addClass('without-header');
-        } else {
-            $('#appHeader').addClass('d-flex');
-            $('#appHeader').removeClass('d-none');
-            $('#message-list').removeClass('without-header');
-            $('#appHeader').removeClass('without-header');
-        }
-    });
-})();
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 		typeof define === 'function' && define.amd ? define(factory) :
