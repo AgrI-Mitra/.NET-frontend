a1b0a1d7faa6500e8289f16033d9abb9d417670b
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2024-08-29 13:05:07 +0530

    Combined logic for auto sound waves and voice based soundh waves

--- kisan-e-mitra/Content/js/bundle-post-body.js
+++ kisan-e-mitra/Content/js/bundle-post-body.js
@@ -819,42 +819,113 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 source.connect(analyser);
 
-                draw(canvas, canvasCtx);
+                /*draw(canvas, canvasCtx);*/
+                stopVisualizer = drawBars(0, canvas, dataArray, null, bufferLength, 1.5, 60, false);
             } catch (e) {
                 console.log('draw error: ', e);
             }
 
         }
 
-        function draw(canvas, canvasCtx) {
-            animationId = requestAnimationFrame(() => draw(canvas, canvasCtx));
+        //function draw(canvas, canvasCtx) {
+        //    animationId = requestAnimationFrame(() => draw(canvas, canvasCtx));
 
-            analyser.getByteFrequencyData(dataArray);
+        //    analyser.getByteFrequencyData(dataArray);
 
-            // Remove the background fill to make it transparent
-            // canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-            // canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-            // Clear the canvas to make the background transparent
-            canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+        //    // Remove the background fill to make it transparent
+        //    // canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+        //    // canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+        //    // Clear the canvas to make the background transparent
+        //    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-            const barWidth = 1.5; // Adjust bar width here
-            let barHeight;
-            let x = 0;
+        //    const barWidth = 1.5; // Adjust bar width here
+        //    let barHeight;
+        //    let x = 0;
 
-            for (let i = 0; i < bufferLength; i++) {
-                barHeight = (dataArray[i] / 255) * canvas.height;
+        //    for (let i = 0; i < bufferLength; i++) {
+        //        barHeight = (dataArray[i] / 255) * canvas.height;
 
-                // Change bar colors here
-                canvasCtx.fillStyle = 'rgb(25, 135, 84)';
+        //        // Change bar colors here
+        //        canvasCtx.fillStyle = 'rgb(25, 135, 84)';
 
-                // Draw upward bars
-                canvasCtx.fillRect(x, canvas.height / 2 - barHeight / 2, barWidth, barHeight / 2);
+        //        // Draw upward bars
+        //        canvasCtx.fillRect(x, canvas.height / 2 - barHeight / 2, barWidth, barHeight / 2);
 
-                // Draw downward bars
-                canvasCtx.fillRect(x, canvas.height / 2, barWidth, barHeight / 2);
+        //        // Draw downward bars
+        //        canvasCtx.fillRect(x, canvas.height / 2, barWidth, barHeight / 2);
 
-                x += barWidth + 1; // Space between bars
+        //        x += barWidth + 1; // Space between bars
+        //    }
+        //}
+
+        function drawBars(
+            timestamp,
+            canvas,
+            dataArray,
+            targetArray,
+            bufferLength,
+            barWidth,
+            fps,
+            isAuto
+        ) {
+            const canvasCtx = canvas.getContext('2d');
+            let lastTime = 0;
+            const interval = 1000 / fps;
+            let animationId;
+
+            function interpolate(current, target, factor) {
+                return current + (target - current) * factor; // Corrected the interpolation logic
             }
+
+            function draw(timestamp) {
+                animationId = requestAnimationFrame(draw);
+
+                const deltaTime = timestamp - lastTime;
+
+                if (deltaTime > interval) {
+                    lastTime = timestamp - (deltaTime % interval);
+
+                    if (isAuto && targetArray) {
+                        // Generate random data to simulate audio frequency data
+                        for (let i = 0; i < bufferLength; i++) {
+                            targetArray[i] = Math.random() * 255;
+                        }
+                    }
+
+                    if (targetArray) {
+                        // Interpolate between current data and target data
+                        for (let i = 0; i < bufferLength; i++) {
+                            dataArray[i] = interpolate(dataArray[i], targetArray[i], 0.1);
+                        }
+                    } else {
+                        analyser.getByteFrequencyData(dataArray); // Get real frequency data
+                    }
+                }
+
+                // Clear the canvas to make the background transparent
+                canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+
+                let barHeight;
+                let x = 0;
+
+                for (let i = 0; i < bufferLength; i++) {
+                    barHeight = (dataArray[i] / 255) * canvas.height;
+
+                    // Change bar colors here
+                    canvasCtx.fillStyle = 'rgb(25, 135, 84)';
+
+                    // Draw upward bars
+                    canvasCtx.fillRect(x, canvas.height / 2 - barHeight / 2, barWidth, barHeight / 2);
+
+                    // Draw downward bars
+                    canvasCtx.fillRect(x, canvas.height / 2, barWidth, barHeight / 2);
+
+                    x += barWidth + 1; // Space between bars
+                }
+            }
+
+            draw(timestamp);
+            return () => cancelAnimationFrame(animationId);
         }
 
         // Stop the visualizer
@@ -883,62 +954,67 @@ document.addEventListener('DOMContentLoaded', function () {
             window.addEventListener('resize', resizeCanvas);
             resizeCanvas();
 
-            const bufferLength = 256; // Number of bars
+            const barWidth = 1.5; // Width of each bar
+            const barSpacing = 0.5; // Space between each bar
+            const totalBarWidth = barWidth + barSpacing;
+            const barsCount = Math.floor(canvas.width / totalBarWidth);
+            const bufferLength = barsCount; // Adjust bufferLength to match barsCount
+
             const dataArray = new Uint8Array(bufferLength);
             const targetArray = new Uint8Array(bufferLength);
 
             let lastTime = 0;
-            const fps = 10; // Frames per second, adjust this value to control speed
+            const fps = 30; // Frames per second, adjust this value to control speed
             const interval = 1000 / fps;
 
-            function interpolate(current, target, factor) {
-                return current + (target - current) * factor;
-            }
+            stopAutoVisualizer = drawBars(0, canvas, dataArray, targetArray, bufferLength, barWidth, fps, true);
 
-            function drawAutoBars(timestamp) {
-                animationId = requestAnimationFrame(drawAutoBars);
+            //function interpolate(current, target, factor) {
+            //    return current + (target - current) * factor;
+            //}
 
-                const deltaTime = timestamp - lastTime;
+            //function drawAutoBars(timestamp) {
+            //    animationId = requestAnimationFrame(drawAutoBars);
 
-                if (deltaTime > interval) {
-                    lastTime = timestamp - (deltaTime % interval);
+            //    const deltaTime = timestamp - lastTime;
 
-                    // Generate random data to simulate audio frequency data
-                    for (let i = 0; i < bufferLength; i++) {
-                        targetArray[i] = Math.random() * 255;
-                    }
-                }
+            //    if (deltaTime > interval) {
+            //        lastTime = timestamp - (deltaTime % interval);
 
-                // Interpolate between current data and target data
-                for (let i = 0; i < bufferLength; i++) {
-                    dataArray[i] = interpolate(dataArray[i], targetArray[i], 0.1);
-                }
+            //        // Generate random data to simulate audio frequency data
+            //        for (let i = 0; i < bufferLength; i++) {
+            //            targetArray[i] = Math.random() * 255;
+            //        }
+            //    }
 
-                // Clear the canvas to make the background transparent
-                canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+            //    // Interpolate between current data and target data
+            //    for (let i = 0; i < bufferLength; i++) {
+            //        dataArray[i] = interpolate(dataArray[i], targetArray[i], 0.1);
+            //    }
 
-                const barWidth = 1.5; // Adjust bar width here
-                let barHeight;
-                let x = 0;
+            //    // Clear the canvas to make the background transparent
+            //    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-                for (let i = 0; i < bufferLength; i++) {
-                    barHeight = (dataArray[i] / 255) * canvas.height;
+            //    let barHeight;
+            //    let x = 0;
 
-                    // Change bar colors here
-                    canvasCtx.fillStyle = 'rgb(25, 135, 84)';
+            //    for (let i = 0; i < bufferLength; i++) {
+            //        barHeight = (dataArray[i] / 255) * canvas.height;
 
-                    // Draw upward bars
-                    canvasCtx.fillRect(x, canvas.height / 2 - barHeight / 2, barWidth, barHeight / 2);
+            //        // Change bar colors here
+            //        canvasCtx.fillStyle = 'rgb(25, 135, 84)';
 
-                    // Draw downward bars
-                    canvasCtx.fillRect(x, canvas.height / 2, barWidth, barHeight / 2);
+            //        // Draw upward bars
+            //        canvasCtx.fillRect(x, canvas.height / 2 - barHeight / 2, barWidth, barHeight / 2);
 
-                    x += barWidth + 1; // Space between bars
-                }
-            }
+            //        // Draw downward bars
+            //        canvasCtx.fillRect(x, canvas.height / 2, barWidth, barHeight / 2);
 
+            //        x += totalBarWidth; // Add space between bars
+            //    }
+            //}
 
-            requestAnimationFrame(drawAutoBars);
+            //drawAutoBars(0); // Start the animation
         }
 
         function stopAutoVisualizer() {
