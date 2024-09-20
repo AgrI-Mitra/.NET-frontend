9627c4f11840b9084a492b1e71eeab48c1cc0568
Himalay Chauhan <himalay.chauhan@tibilsolutions.com>
2024-09-20 16:55:59 +0530

    Securing cookies if they are not

---
+++ kisan-e-mitra/App_Start/SecureCookiesAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KisanEMitra
+{
+    public class SecureCookiesAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var cookies = new[] { "_ga", "_ga_RCEZR4J24G", "culture" };
+
+            foreach (var cookieName in cookies)
+            {
+
+                var cookie = filterContext.HttpContext.Request.Cookies[cookieName];
+
+                if (cookie != null && !cookie.Secure)
+                {
+                    cookie.HttpOnly = true;
+                    cookie.Secure = true;
+
+                    filterContext.HttpContext.Response.Cookies.Add(cookie);
+                }
+            }
+            base.OnActionExecuted(filterContext);
+        }
+    }
+
+}
--- kisan-e-mitra/Models/LanguageManager.cs
+++ kisan-e-mitra/Models/LanguageManager.cs
@@ -90,8 +90,12 @@ namespace KisanEMitra.Models
                 var cultureInfo = new CultureInfo(lang);
                 Thread.CurrentThread.CurrentUICulture = cultureInfo;
                 Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(cultureInfo.Name);
-                HttpCookie langCookie = new HttpCookie("culture", lang);
-                langCookie.Expires = DateTime.Now.AddYears(1);
+                HttpCookie langCookie = new HttpCookie("culture", lang)
+                {
+                    Expires = DateTime.Now.AddYears(1),
+                    Secure = true,
+                    HttpOnly = true
+                };
                 HttpContext.Current.Response.Cookies.Add(langCookie);
             }
             catch (Exception) { }
